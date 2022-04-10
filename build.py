7aa608b03fcace68908ba80afafe9131200e3697
Gideon Girigiri <gideongirigiri@gamil.com>
2022-04-10 04:22:29 -0400

    please work

--- build.py
+++ build.py
@@ -1,2 +1,21 @@
+from ast import If
 import os
-result = os.system("sfdx force:org:list")
+import sys
+
+
+# sfdx force: auth: jwt: grant - u gideongirigiri@gmail.com - f cert/server.key - i 3MVG9dZJodJWITSugONOAAWoxjDmR23_N0fta65n9ldUIhyCZuqxERnOAj8BOrsMJndLMFXL5s.p4SC7.uWd5\n - r https: // login.salesforce.com
+
+result = os.system("sfdx force:auth:jwt:grant -u gideongirigiri@gmail.com -f cert/server.key -i 3MVG9dZJodJWITSugONOAAWoxjDmR23_N0fta65n9ldUIhyCZuqxERnOAj8BOrsMJndLMFXL5s.p4SC7.uWd5 -r https://login.salesforce.com")
+
+
+os.system(
+    "sfdx force:apex:test:run -u gideongirigiri@gmail.com --outputdir test_result")
+
+f = open("./test_result/test-run-id.txt")
+reportId = f.read()
+
+code = os.system("sfdx force:apex:test:report -i " +
+                 reportId + " -u gideongirigiri@gmail.com --resultformat human")
+
+if code != 0:
+    sys.exit(1)
