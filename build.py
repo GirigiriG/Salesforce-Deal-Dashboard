43d23fdcaf06fd70a04c50d789367105944f30ac
Gideon Girigiri <gideongirigiri@gamil.com>
2022-04-10 04:56:42 -0400

    getenv username

--- build.py
+++ build.py
@@ -5,17 +5,18 @@ import sys
 
 # sfdx force: auth: jwt: grant - u gideongirigiri@gmail.com - f cert/server.key - i 3MVG9dZJodJWITSugONOAAWoxjDmR23_N0fta65n9ldUIhyCZuqxERnOAj8BOrsMJndLMFXL5s.p4SC7.uWd5\n - r https: // login.salesforce.com
 
-result = os.system("sfdx force:auth:jwt:grant -u gideongirigiri@gmail.com -f cert/server.key -i 3MVG9dZJodJWITSugONOAAWoxjDmR23_N0fta65n9ldUIhyCZuqxERnOAj8BOrsMJndLMFXL5s.p4SC7.uWd5 -r https://login.salesforce.com")
+# result = os.system("sfdx force:auth:jwt:grant -u gideongirigiri@gmail.com -f cert/server.key -i 3MVG9dZJodJWITSugONOAAWoxjDmR23_N0fta65n9ldUIhyCZuqxERnOAj8BOrsMJndLMFXL5s.p4SC7.uWd5 -r https://login.salesforce.com")
 
 
-os.system(
-    "sfdx force:apex:test:run -u gideongirigiri@gmail.com --outputdir test_result")
+# os.system(
+#     "sfdx force:apex:test:run -u gideongirigiri@gmail.com --outputdir test_result")
 
-f = open("./test_result/test-run-id.txt")
-reportId = f.read()
+# f = open("./test_result/test-run-id.txt")
+# reportId = f.read()
 
-code = os.system("sfdx force:apex:test:report -i " +
-                 reportId + " -u gideongirigiri@gmail.com --resultformat human")
+# code = os.system("sfdx force:apex:test:report -i " +
+#                  reportId + " -u gideongirigiri@gmail.com --resultformat human")
 
-if code == 0:
-    sys.exit(1)
+print(os.getenv("USERNAME"))
+# if code == 0:
+#     sys.exit(1)
